25696da0a055e5eb3d998900d6674c7936780f7d
Siel <juliandavid347@gmail.com>
2014-08-30 12:39:50 -0500

    Bases de datos y content provider
    Funcionando bases de datos //quitar el comentario para activar la
    inyección por base de datos
    
    funcionando content provider //actualmente habilitado

--- src/com/example/nuevoyamba/RefreshService.java
+++ src/com/example/nuevoyamba/RefreshService.java
@@ -9,12 +9,12 @@ import android.app.IntentService;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.util.Log;
 import android.widget.Toast;
-import java.util.List;
 
 
 
@@ -34,7 +34,7 @@ public class RefreshService extends IntentService {
 	@Override
 	protected void onHandleIntent(Intent intent) { 
 		SharedPreferences prefs = PreferenceManager
-				.getDefaultSharedPreferences(this); // 
+				.getDefaultSharedPreferences(this); 
 		final String username = prefs.getString("username", "");
 		final String password = prefs.getString("password", "");
 		if (TextUtils.isEmpty(username) || TextUtils.isEmpty(password)) {
@@ -44,21 +44,45 @@ public class RefreshService extends IntentService {
 			return;
 		}
 		Log.d(TAG, "onStarted");
+		DbHelper dbHelper = new DbHelper(this);
+		SQLiteDatabase db = dbHelper.getWritableDatabase();
+		ContentValues values = new ContentValues();
 		String apiroot = prefs.getString("apiroot","http://yamba.marakana.com/api");
 		twitter=new Twitter(username,password);
 		twitter.setAPIRootUrl(apiroot); 
 		try {
-			 List<Status> timeline = twitter.getUserTimeline(); // 
-			 for (Status status : timeline) { // 
-			 Log.d(TAG,
-			 String.format("%s: %s", status.getUser(),
-			 status.getText())); // 
-			 }
-			 } catch (TwitterException e) { // 
-			 Log.e(TAG, "Failed to fetch the timeline", e);
-			 e.printStackTrace();
-			 }
-			 return;
+			int count = 0;
+			List<Status> timeline = twitter.getUserTimeline(); 
+			for (Status status : timeline) { 
+				//pongo todos los valores de cada uno de los estados en una especie de arreglo
+				values.clear();
+				values.put(StatusContract.Column.ID,status.getId());
+				values.put(StatusContract.Column.USER,status.getUser().name);
+				values.put(StatusContract.Column.MESSAGE,status.getText());
+				values.put(StatusContract.Column.CREATED_AT,status.getCreatedAt().getTime());
+				//para luego insertarlo en la base de datos
+				//insercción directa
+				//db.insert(StatusContract.TABLE, null, values);
+
+				//insercción a através de content provider
+				Uri uri = getContentResolver().insert(
+						StatusContract.CONTENT_URI, values); // 
+				if (uri != null) {
+					count++; // 
+					Log.d(TAG,
+							String.format("%s: %s", status.getUser(),
+									status.getText()));
+				}
+
+				Log.d(TAG,
+						String.format("%s: %s", status.getUser(),
+								status.getText())); 
+			}
+		} catch (TwitterException e) { 
+			Log.e(TAG, "Failed to fetch the timeline", e);
+			e.printStackTrace();
+		}
+		return;
 	}
 
 	@Override
